45c5903521b6f108915e4fe0ddde06144948cfdf
Karlson <m.norets@gmail.com>
2020-02-28 23:30:05 +0200

    Updates

---
+++ precache-manifest.7cfed758d93cd7dfe136d59170eeca91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a1f47cb2f9f7f47421cf8ac05b1a1d",
+    "url": "/auth_test/./index.html"
+  },
+  {
+    "revision": "944b3f368e62c29b855e",
+    "url": "/auth_test/./static/js/2.de8c3331.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/auth_test/./static/js/2.de8c3331.chunk.js.LICENSE"
+  },
+  {
+    "revision": "491c0d8e4a5680c8ff46",
+    "url": "/auth_test/./static/js/main.4a21241f.chunk.js"
+  },
+  {
+    "revision": "0355439f8f76e6f7f1b5",
+    "url": "/auth_test/./static/js/runtime-main.eea23eef.js"
+  }
+]);
