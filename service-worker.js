8cb13c122563488b6b33431c5b28ffc99db5a51f
Karlson <m.norets@gmail.com>
2020-03-02 11:48:59 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/auth_test/./precache-manifest.cecfb342da9cad73e591ee28eec2f07e.js"
+  "/auth_test/./precache-manifest.3af5a6da12d453761668630a91dffe56.js"
 );
 
 self.addEventListener('message', (event) => {
